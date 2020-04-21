d4ad4068749be272489e49855ea27c7e53111eda
Lucas Martioli <martiolilucas@gmail.com>
2020-04-21 20:53:44 -0300

    Iniciando projeto de Taxa de Juros

---
+++ TaxaJurosAPI/Controllers/TaxaJurosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxaJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public double Get()
+        {
+            return TaxasDeJuro.TaxaFixa;
+        }
+    }
+}
---
+++ TaxaJurosAPI/Models/TaxasDeJuro.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaxaJurosAPI
+{
+    public static class TaxasDeJuro
+    {
+        private const double taxaFixa = 0.01d;
+
+        public static double TaxaFixa => taxaFixa;
+    }
+}
